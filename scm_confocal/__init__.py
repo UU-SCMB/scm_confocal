394c8f641571fd0ef3107ad9d20ff137fe7a535e
Maarten <54527765+MaartenBransen@users.noreply.github.com>
2021-02-01 16:56:50 +0100

    add sp8_lif to init

--- scm_confocal/__init__.py
+++ scm_confocal/__init__.py
@@ -1,11 +1,12 @@
 __version__ = '1.1'
 
-from .sp8 import sp8_series
+from .sp8 import sp8_lif,sp8_series
 from .visitech import visitech_series,visitech_faststack
 from .util import util
 
 #make available when using 'from scm_confocal import *'
 __all__ = [
+    'sp8_lif',
     'sp8_series',
     'visitech_series',
     'visitech_faststack',
