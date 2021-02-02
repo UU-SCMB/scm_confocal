f03de8394148d560cc44e1ba392232c6e7d98b93
Maarten <54527765+MaartenBransen@users.noreply.github.com>
2021-02-02 09:42:36 +0100

    add sp8_lif to __init__

--- scm_confocal/__init__.py
+++ scm_confocal/__init__.py
@@ -1,10 +1,6 @@
 __version__ = '2.0.0'
 
-<<<<<<< Updated upstream
 from .sp8 import sp8_lif,sp8_series
-=======
-from .sp8 import sp8_series,sp8_lif
->>>>>>> Stashed changes
 from .visitech import visitech_series,visitech_faststack
 from .util import util
 
@@ -12,7 +8,6 @@ from .util import util
 __all__ = [
     'sp8_lif',
     'sp8_series',
-    'sp8_lif',
     'visitech_series',
     'visitech_faststack',
     'util'
