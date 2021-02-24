9fcff8f5628109d5e6a7216fcbff7809118a88a9
Maarten <54527765+MaartenBransen@users.noreply.github.com>
2021-02-24 13:30:37 +0100

    Update sp8.py

--- scm_confocal/sp8.py
+++ scm_confocal/sp8.py
@@ -36,11 +36,10 @@ class sp8_lif:
     
     See also
     --------
-    sp8_image() : 
-        subclass for specific images in the dataset.
-    readlif : 
-        the library used for acessing the files, which can be found at 
-        https://github.com/nimne/readlif.
+    * `sp8_image()`, a subclass for specific images in the dataset.
+    * [`readlif`](https://github.com/nimne/readlif), the library used for 
+    acessing the files.
+        
     """
     def __init__(self,filename,quiet=False):
         """
