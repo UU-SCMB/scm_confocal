d43377c080bf078684f83d1dd075403f7d26e71f
Maarten <54527765+MaartenBransen@users.noreply.github.com>
2021-02-24 12:49:52 +0100

    fix docstring

--- scm_confocal/sp8.py
+++ scm_confocal/sp8.py
@@ -36,11 +36,9 @@ class sp8_lif:
     
     See also
     --------
-    sp8_image()
-        subclass for specific images in the dataset
-    readlif
-        the library used for acessing the files, which can be found at 
-        https://github.com/nimne/readlif
+    sp8_image() : subclass for specific images in the dataset
+    readlif : the library used for acessing the files, which can be found at 
+    https://github.com/nimne/readlif
     """
     def __init__(self,filename,quiet=False):
         """
