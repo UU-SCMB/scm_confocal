4ddef4abf610ec9765dcb0ba8f6bdef67d9a7823
Maarten <54527765+MaartenBransen@users.noreply.github.com>
2021-02-24 09:43:40 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="scm_confocal",
+    version="2.0.0",
+    author="Maarten Bransen",
+    author_email="m.bransen@uu.nl",
+    license='GNU General Public License v3.0',
+    long_description=open('README.md').read(),
+    packages=find_packages(include=["scm_confocal", "scm_confocal.*"]),
+    install_requires=[
+        "numpy>=1.19.2",
+        "scipy>=1.6.0",
+        "pillow>=6.2.1",
+        "pims>=0.5"
+    ],
+)
