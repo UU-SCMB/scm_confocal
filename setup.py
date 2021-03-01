95ed47a88e93ff96e972713b0fe0bad228505d96
Maarten <54527765+MaartenBransen@users.noreply.github.com>
2021-03-01 17:17:26 +0100

    increment version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="scm_confocal",
-    version="1.1.0",
+    version="1.1.1",
     author="Maarten Bransen",
     author_email="m.bransen@uu.nl",
     license='GNU General Public License v3.0',
